51a30cf71234b0d220cc5ccc9a0dfca1ddbd5999
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-06-14 00:48:06 +0700

    config login google firebase

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Login, Home } from "../screens";
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName='Login'
+    >
+      <Stack.Screen 
+        name="Login" 
+        component={Login}
+        options={{
+          headerShown: false
+        }}
+      />
+      <Stack.Screen 
+        name="Home" 
+        component={Home}
+        options={{
+          headerShown: false
+        }}
+      />
+    </Stack.Navigator>
+  )
+}
+
+export default Router;
